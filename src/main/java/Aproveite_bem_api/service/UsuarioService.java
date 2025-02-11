12e733c3cb558f3fae2d659279334d9e2a350e11
solmazza <solmazzarolo@gmail.com>
2025-02-10 22:07:08 -0300

    commit novo

--- src/main/java/Aproveite_bem_api/service/UsuarioService.java
+++ src/main/java/Aproveite_bem_api/service/UsuarioService.java
@@ -1,7 +1,24 @@
 package Aproveite_bem_api.service;
 
+import Aproveite_bem_api.config.ConfigSenhas;
+import Aproveite_bem_api.model.Usuario;
+import Aproveite_bem_api.repository.UsuarioRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioService {
+    private final UsuarioRepository usuarioRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public UsuarioService(UsuarioRepository usuarioRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.usuarioRepository = usuarioRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public Usuario cadastrar(Usuario usuario) {
+        usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
+        return usuarioRepository.save(usuario);
+    }
+
 }
