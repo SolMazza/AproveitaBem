5a4fac34bbbc3024d1916e21fef9102e9119f81a
Sol Mazza <145726469+SolMazza@users.noreply.github.com>
2025-03-06 18:21:18 -0300

    Add files via upload

---
+++ Back_end_api/aproveite_bem_api/src/main/java/aproveite_bem/aproveite_bem_api/AproveiteBemApiApplication.java
@@ -0,0 +1,13 @@
+package aproveite_bem.aproveite_bem_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AproveiteBemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AproveiteBemApiApplication.class, args);
+	}
+
+}
---
+++ Back_end_api/aproveite_bem_api/src/test/java/aproveite_bem/aproveite_bem_api/AproveiteBemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package aproveite_bem.aproveite_bem_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AproveiteBemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
