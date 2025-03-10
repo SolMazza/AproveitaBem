1e45dad626190a87acb251014b75bb21793c3ab8
SolMazza <solmazzarolo@gmail.com>
2025-03-10 16:03:20 -0300

    commit de teste

--- Back_end_api/aproveite_bem_api/src/main/java/aproveite_bem/service/ProdutoService.java
+++ Back_end_api/aproveite_bem_api/src/main/java/aproveite_bem/service/ProdutoService.java
@@ -1,7 +1,23 @@
 package aproveite_bem.service;
 
+import aproveite_bem.model.Produto;
+import aproveite_bem.repository.ProdutoRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProdutoService {
+
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoService(ProdutoRepository produtoRepository){
+        this.produtoRepository = produtoRepository;
+    }
+
+    public List<Produto> listaTodosOsProdutos(){
+        List<Produto> produtos = produtoRepository.findAll();
+        return produtos;
+    }
+
 }
