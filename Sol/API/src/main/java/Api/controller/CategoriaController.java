01fa526f55eac5b55b221802c28e699e0d643f6e
Antonia Beber <86435535+AntoniaBeber@users.noreply.github.com>
2025-03-17 19:24:47 -0300

    Update CategoriaController.java

--- Sol/API/src/main/java/Api/controller/CategoriaController.java
+++ Sol/API/src/main/java/Api/controller/CategoriaController.java
@@ -1,10 +1,13 @@
 package Api.controller;
 
 import Api.model.Categoria;
+import Api.model.Produto;
 import Api.service.CategoriaService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(path = "categorias")
@@ -26,13 +29,31 @@ public class CategoriaController {
     return ResponseEntity.ok(categoria);
 
     }
+    @GetMapping()
+        public List<Categoria> ListarTodos(){
+        List<Categoria> categorias = categoriaService.ListarTodos();
+        return categorias;
+    }
+
+    @GetMapping()
+        public List<Categoria> ListarNomes(@RequestParam String nome){
+        List<Categoria> categorias = categoriaService.ListarNome(nome);
+        return categorias;
+    }
+    @DeleteMapping()
+    public ResponseEntity<String> deletarTodos(){
+    categoriaService.deletarTodos();
+    return ResponseEntity.ok("Categoria deletada com sucesso");
+    }
+
+    @DeleteMapping("/id")
+    public ResponseEntity<String> deletar(@RequestParam Long id){
+    categoriaService.deletarPeloId((id));
+    return ResponseEntity.ok("Categoria deletada com sucesso!");
+    }
+    @PutMapping("/editar")
+    public ResponseEntity<Categoria> editar(@RequestBody Long id, Categoria categoria){
+    return ResponseEntity.ok(categoriaService.editar(id, categoria));
+    }
+
 }
-/*
-public List<Categoria> ListarTodos(){
-public List<Categoria> ListarNome(String nome){
-public Categoria buscarPeloId(Long id) {
-public void deletarTodos()
-public void deletarPeloId(Long id) {
-
-
-*/
