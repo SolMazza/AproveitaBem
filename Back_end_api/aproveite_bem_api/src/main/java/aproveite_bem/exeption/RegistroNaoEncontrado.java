9c493cd8f66a5cdeb0866e43a3c082504f775eb5
Sol Mazza <145726469+SolMazza@users.noreply.github.com>
2025-03-17 18:51:19 -0300

    Delete Back_end_api directory

--- Back_end_api/aproveite_bem_api/src/main/java/aproveite_bem/exeption/RegistroNaoEncontrado.java
+++
@@ -1,8 +0,0 @@
-package aproveite_bem.exeption;
-
-public class RegistroNaoEncontrado extends RuntimeException{
-    public RegistroNaoEncontrado(String mensagem){
-        super(mensagem);
-    }
-
-}
